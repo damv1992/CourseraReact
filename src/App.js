886f5d5d8ec87e731a39dbb90104354a43399dac
Daniel Alejandro Miranda Villalta <daniel.alejandro.miranda.villalta@gmail.com>
2024-09-16 18:16:48 -0400

    Eventos dinámicos

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import "./App.css";
-import Card from "./Card";
-
 function App() {
-return (
-    <div className="App">
-        <h1>Task: Add three Card elements</h1>
-        <Card h2="First card's h2" h3="First card's h3" />
-        <Card h2="Second card's h2" h3="Second card's h3" />
-        <Card h2="Third card's h2" h3="Third card's h3" />
+  function handleClick() {
+    let randomNum = Math.floor(Math.random() * 3) + 1;
+    console.log(randomNum);
+    let userInput = prompt('type a number');
+    alert(`Computer number: ${randomNum}, Your guess: ${userInput}`);
+  }
+
+  return (
+    <div>
+      <h1>Task: Add a button and handle a click event</h1>
+      <button onClick={handleClick}>Guess the number between 1 and 3</button>
     </div>
   );
-};
+}
 
 export default App;
